79c14b19d1e8453a773f13b8e17e20bf5e0b81c2
Majid <mghafari41@yahoo.com>
2022-12-20 00:01:12 -0800

    DiscountApi implementation completed

---
+++ src/Sevices/Basket/Basket.API/Data/IShoppingCartContext.cs
@@ -0,0 +1,28 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Basket.API.Data
+{
+    public interface IShoppingCartContext
+    {
+        IMongoCollection<ShoppingCart> ShoppingCarts { get; }
+    }
+
+    public class ShoppingCartContext : IShoppingCartContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ShoppingCartContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var client = new MongoClient(_configuration.GetValue<string>("DatabaseSettings:Connectionstring"));
+            var database = client.GetDatabase(_configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            ShoppingCarts = database.GetCollection<ShoppingCart>(_configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        }
+
+        public IMongoCollection<ShoppingCart> ShoppingCarts { get; }
+    }
+}
+
+
