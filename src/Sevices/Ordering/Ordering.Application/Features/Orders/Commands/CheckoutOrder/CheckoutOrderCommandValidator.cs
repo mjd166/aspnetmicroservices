816fcb4d4d27f0205873e000d1dde289538f08be
Majid <mghafari41@yahoo.com>
2022-12-24 01:57:04 -0800

    CheckoutOrderCommand of Orderin.Applicationlayer impleneted with CQRS pattern .

---
+++ src/Sevices/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator:AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{userName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{userName} must not exceed 50 characters.");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+        }
+    }
+}
