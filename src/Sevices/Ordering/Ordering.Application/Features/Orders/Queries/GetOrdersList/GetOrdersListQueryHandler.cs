699fc4174f452d456efbc212b765ab028c58b7a1
Majid <mghafari41@yahoo.com>
2022-12-24 00:33:00 -0800

    Application Feature of Ordering.ApplicationLayer implemented with CQRS and MediatR.

---
+++ src/Sevices/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async System.Threading.Tasks.Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+           var orderList= await _orderRepository.GetOrdersByUserName(request.userName);
+           return  _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
--- src/Sevices/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Sevices/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,8 +1,14 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings
 {
     public class MappingProfile: Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+        }
     }
 }
