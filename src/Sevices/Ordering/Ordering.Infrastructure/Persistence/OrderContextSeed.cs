cd7103cfa1f6b46a79ec34ec1505ec531f2b5eb3
Majid <mghafari41@yahoo.com>
2022-12-25 01:03:37 -0800

    OrderContext and OrderContextSeed implemented on infrastructure layer .

---
+++ src/Sevices/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreConfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("seed database associated with context {DbContextName}", typeof(Order).Name);
+            }
+        }
+
+        public static IEnumerable<Order> GetPreConfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order(){UserName ="swm",FirstName ="Majid",LastName ="Ghafari",EmailAddress="mghafari41@yahoo.com",Country ="Iran",TotalPrice =550}
+            };
+
+        }
+    }
+}
