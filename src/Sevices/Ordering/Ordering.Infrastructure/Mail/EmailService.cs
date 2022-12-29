5be1a80f393601f9188f4e92b0d2b876ba901436
Majid <mghafari41@yahoo.com>
2022-12-28 23:17:07 -0800

    BaseRespository and Order Repository and Email Sending Implemented in Infrastructure layer

---
+++ src/Sevices/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Ordering.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+
+        public EmailSettings _emailSettings { get; }
+        public ILogger<EmailService> _logger { get; }
+
+        public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
+        {
+            _emailSettings = emailSettings.Value;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendMail(Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+            var from = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            var response = await client.SendEmailAsync(sendGridMessage);
+            _logger.LogInformation($"Email Sent");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+                return true;
+
+            _logger.LogError("Email Sending Failed.");
+
+            return false;
+        }
+    }
+}
---
+++ src/Sevices/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+
+        public OrderRepository(OrderContext dbContext) :base(dbContext)
+        {
+
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await  _orderContext.Orders
+                                                .Where(o => o.UserName == userName)
+                                                .ToListAsync();
+            return orderList;
+        }
+    }
+}
